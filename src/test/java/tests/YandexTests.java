225b2cfcab3170a552cfc1a2f6b84bd12b85c035
Vasenkov Stanislav <aanher@gmail.com>
2020-08-25 22:18:36 +0300

    Update and rename YandexTest to YandexTests.java
    Code review #1

--- src/test/java/tests/YandexTests.java
+++ src/test/java/tests/YandexTests.java
@@ -7,14 +7,14 @@ import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class YandexTest {
+public class YandexTests {
     @Test
     public void selenideSearchAtYandexTest() {
         // Открыть yandex
         open("https://yandex.ru");
 
         // Ввести Selenide в поиск
-        $(byId("text")).setValue("Selenide").pressEnter();
+        $("#text").setValue("Selenide").pressEnter();
 
         // Проверить, что Selenide появился в результатах поиска
         $("html").shouldHave(text("ru.selenide.org"));
