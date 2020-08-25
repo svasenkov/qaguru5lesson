9cce6969756c9c2434a3d0ed4775d4fae4354d58
Vasenkov Stanislav <aanher@gmail.com>
2020-08-25 22:17:46 +0300

    Update and rename GoogleTest.java to GoogleTests.java
    Code review

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-class GoogleTest {
+class GoogleTests {
     @Test
     void selenideSearchTest() {
         // Открыть google
